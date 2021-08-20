0ab3d35519662c6e704ce3df414808f161c1095b
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2021-08-20 17:03:18 +0900

    update java client for tests

--- locustfiles/locustfile_java.py
+++ locustfiles/locustfile_java.py
@@ -6,16 +6,16 @@ from locust import HttpUser, task, between, tag
 from locust.contrib.fasthttp import FastHttpUser
 
 
-class LinkingTester(FastHttpUser):
+class GrobidSuperconductorsTester(HttpUser):
     wait_time = between(5, 9)
 
-    paragraphs = []
+    json_documents = []
     pdf_documents = []
     text_documents = []
 
     @tag('process_pdfs')
     @task
-    def classify_temperature(self):
+    def process_pdfs(self):
         n = random.randint(0, len(self.pdf_documents) - 1)
         pdf_document = self.pdf_documents[n]
 
@@ -29,39 +29,54 @@ class LinkingTester(FastHttpUser):
         }
 
         headers = {"Accept": "application/json"}
-        self.client.post(path="/service/process/pdf", data=files, headers=headers, name="/service/process/pdf")
+        self.client.post("/service/process/pdf", files=files, headers=headers, name="/service/process/pdf")
 
-    @tag('process_text')
-    @task
-    def process_text(self):
-        n = random.randint(0, len(self.text_documents) - 1)
-        text_document = self.text_documents[n]
+    # @tag('process_text')
+    # @task
+    # def process_text(self):
+    #     n = random.randint(0, len(self.text_documents) - 1)
+    #     text_document = self.text_documents[n]
+    #
+    #     headers = {"Accept": "application/json"}
+    #     files = {"text": text_document}
+    #     self.client.post("/service/process/text", files=files, headers=headers, name="/service/process/text")
 
-        headers = {"Accept": "application/json"}
-        files = {"text": text_document}
-        self.client.post(path="/service/process/text", data=files, headers=headers, name="/service/process/text")
-
-    @tag('process_json')
-    @task
-    def process_json(self):
-        n = random.randint(0, len(self.paragraphs) - 1)
-        paragraph = self.paragraphs[n]
-
-        headers = {"Accept": "application/json"}
-        files = {"input": json.dumps(paragraph)}
-        self.client.post(path="/service/process/json", data=files, headers=headers, name="/service/process/json")
+    # @tag('process_json')
+    # @task
+    # def process_json(self):
+    #     n = random.randint(0, len(self.paragraphs) - 1)
+    #     paragraph = self.paragraphs[n]
+    #
+    #     headers = {"Accept": "application/json"}
+    #     files = {"input": json.dumps(paragraph)}
+    #     self.client.post(path="/service/process/json", data=files, headers=headers, name="/service/process/json")
 
 
     def on_start(self):
-        for root, dirs, files in os.walk("resources/data/pdfs/"):
-            for file_ in files:
-                if file_.lower().endswith(".pdf"):
-                    abs_path = os.path.join(root, file_)
-                    self.pdf_documents.add(abs_path)
+        if len(self.pdf_documents) == 0:
 
-        for root, dirs, files in os.walk("resources/data/texts/"):
-            for file_ in files:
-                if file_.lower().endswith(".txt"):
-                    abs_path = os.path.join(root, file_)
-                    self.text_documents.add(abs_path)
+            # for root, dirs, files in os.walk("resources/data/pdfs/"):
+            for root, dirs, files in os.walk("/Users/lfoppiano/development/projects/grobid/grobid-superconductors/resources/dataset/superconductors/corpus/pdf/batches"):
+                for file_ in files:
+                    if file_.lower().endswith(".pdf"):
+                        abs_path = os.path.join(root, file_)
+                        self.pdf_documents.append(abs_path)
 
+        # if len(self.text_documents) == 0:
+        #     for root, dirs, files in os.walk("resources/data/texts/"):
+        #         for file_ in files:
+        #             if file_.lower().endswith(".txt"):
+        #                 abs_path = os.path.join(root, file_)
+        #                 with open(abs_path, 'r') as text_file:
+        #                     for line in text_file:
+        #                         self.text_documents.append(line.strip())
+
+
+        # if len(self.json_documents) == 0:
+        #     for root, dirs, files in os.walk("resources/data/json/"):
+        #         for file_ in files:
+        #             if file_.lower().endswith(".json"):
+        #                 abs_path = os.path.join(root, file_)
+        #                 with open(abs_path, 'r') as text_file:
+        #                     document = json.load(text_file)
+        #                     self.paragraphs.extend(sentences)
